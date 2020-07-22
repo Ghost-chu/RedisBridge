c17c6e2fc356cc0d7a781ecd44eb2b920633f020
Ghost_chu <2908803755@qq.com>
2020-07-22 19:56:24 +0800

    Bug fix

--- RedisBridge-common/src/main/java/com/mcsunnyside/redisbridge/common/bus/Subscribe.java
+++ RedisBridge-common/src/main/java/com/mcsunnyside/redisbridge/common/bus/Subscribe.java
@@ -1,19 +1,15 @@
 package com.mcsunnyside.redisbridge.common.bus;
 
 
-import org.jetbrains.annotations.NotNull;
+import java.lang.annotation.*;
 
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
-
-@Target(ElementType.METHOD)
+@Documented
 @Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+@Inherited
 public @interface Subscribe {
     /**
      * The channel id you want to register
      */
-    @NotNull
-    String channel = "default";
+    String channel() default "default";
 }
